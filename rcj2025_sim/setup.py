c065747dbc648f043f48af528fd388921bb36efe
Kengokuma <kengo171227@gmail.com>
2025-08-19 17:44:26 +0900

    feat: spawn gazebo sim

---
+++ rcj2025_sim/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'rcj2025_sim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kengo',
+    maintainer_email='kengo171227@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
